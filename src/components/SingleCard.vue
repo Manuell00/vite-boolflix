89197518d648a800d731409e42cba5b463378350
Manuell00 <manuell.caselli@gmail.com>
2023-06-08 13:50:53 +0200

    Inserimento bandiere + eccezzione

--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -1,28 +1,55 @@
 <script>
+// Importo lo STORE
+import { store } from './../store.js';
+import CountryFlag from 'vue-country-flag-next'
+
+
 // Inserisco l'EXPORT
 export default {
     name: 'SingleCard',
     props: {
         details: Object
+    },
+
+    components: {
+        CountryFlag
+    },
+
+    methods: {
+        isLanguageInCountries(language) {
+            return store.countries.includes(language);
+        },
+    },
+
+    data() {
+        return {
+            store
+        }
     }
 }
 </script>
 
 
-<!-- TEMPLATE -->
 <template>
     <!-- Card di esempio -->
     <div class="text-center card mb-3">
         <div class="cardCharacter">
-
-            <div class="info"> <span class="bold">Titolo :</span> {{ details.title }}</div>
-            <div class="info"> <span class="bold">Titolo Completo :</span> {{ details.original_title }}
-            </div>
+            <div class="info"><span class="bold">Titolo :</span> {{ details.title }}</div>
+            <div class="info"><span class="bold">Titolo Completo :</span> {{ details.original_title }}</div>
             <div class="info"><span class="bold">Voto :</span> {{ details.vote_average }}</div>
-            <div class="info"> <span class="bold">Lingua :</span> {{ details.original_language }}</div>
+            <div class="info">
+                <span class="bold">Lingua :</span>
+                <country-flag v-if="details.original_language === 'en'" :country="'gb-eng'" />
+                <country-flag v-else-if="isLanguageInCountries(details.original_language)"
+                    :country="details.original_language" />
+                <template v-else>
+                    {{ details.original_language }}
+                </template>
+            </div>
         </div>
     </div>
 </template>
+
 
 
 <!-- STYLE -->
@@ -43,7 +70,6 @@ export default {
             text-align: center;
             height: 90px;
 
-            .info {}
         }
 
         .bold {
--- src/store.js
+++ src/store.js
@@ -11,6 +11,8 @@ export const store = reactive({
     charactersList: [],
 
     // Variabile per ricercare valore
-    searchText: ""
+    searchText: "",
+
+    countries: ['af', 'ax', 'al', 'dz', 'as', 'ad', 'ao', 'ai', 'aq', 'ag', 'ar', 'am', 'aw', 'au', 'at', 'az', 'bs', 'bh', 'bd', 'bb', 'by', 'be', 'bz', 'bj', 'bm', 'bt', 'bo', 'bq', 'ba', 'bw', 'bv', 'br', 'io', 'bn', 'bg', 'bf', 'bi', 'cv', 'kh', 'cm', 'ca', 'ky', 'cf', 'td', 'cl', 'cn', 'cx', 'cc', 'co', 'km', 'cg', 'cd', 'ck', 'cr', 'ci', 'hr', 'cu', 'cw', 'cy', 'cz', 'dk', 'dj', 'dm', 'do', 'ec', 'eg', 'sv', 'gq', 'er', 'ee', 'et', 'fk', 'fo', 'fj', 'fi', 'fr', 'gf', 'pf', 'tf', 'ga', 'gm', 'ge', 'de', 'gh', 'gi', 'gr', 'gl', 'gd', 'gp', 'gu', 'gt', 'gg'],
 
 });
